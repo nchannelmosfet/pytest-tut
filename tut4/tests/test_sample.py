c2a7bf1e2c0558f647dc3f3796a262d45d394203
JerryWu <jwu@digitalairstrike.com>
2021-07-26 19:19:54 -0700

    Update test_sample.py

--- tut4/tests/test_sample.py
+++ tut4/tests/test_sample.py
@@ -15,8 +15,14 @@ def test_add_list():
     assert add([1, 2], [3]) == [1, 2, 3]
 
 
-@pytest.mark.parametrize("a,b,c", [(1, 2, 3), ("a", "b", "ab"),
-                                   ([1, 2], [3], [1, 2, 3])],
-                         ids=["num", "str", "list"])
+@pytest.mark.parametrize(
+    "a,b,c",
+    [
+        (1, 2, 3),
+        ("a", "b", "ab"),
+        ([1, 2], [3], [1, 2, 3])
+    ],
+    ids=["num", "str", "list"]
+)
 def test_add(a, b, c):
     assert add(a, b) == c
